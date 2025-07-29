ef6cac054f444fa67c05c15f99fab909cb5b0593
Charlie Dahle <charliedahle161@gmail.com>
2025-07-28 23:32:08 -0400

    trying to fix refresh issue

--- drum-machine/src/main.jsx
+++ drum-machine/src/main.jsx
@@ -1,63 +1,65 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion'
-import App from './App.jsx'
-import DrumMachineApp from './components/DrumMachineApp/DrumMachineApp.jsx'
-import Home from './pages/Home/Home.jsx'
-import AnimatedPage from './AnimatedPage.jsx'
-import './styles/index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import App from "./App.jsx";
+import DrumMachineApp from "./components/DrumMachineApp/DrumMachineApp.jsx";
+import Home from "./pages/Home/Home.jsx";
+import AnimatedPage from "./AnimatedPage.jsx";
+import "./styles/index.css";
 
 function AppRouter() {
-  const location = useLocation()
-  
+  const location = useLocation();
+
   return (
-    <div style={{ position: 'relative', minHeight: '100vh' }}>
+    <div style={{ position: "relative", minHeight: "100vh" }}>
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
-          <Route path="/" 
-          element={<AnimatedPage> 
-            <Home />  {/* Blob stack homepage! */}
+          <Route
+            path="/"
+            element={
+              <AnimatedPage>
+                <Home /> {/* Blob stack homepage! */}
               </AnimatedPage>
-            } 
-            />
-          <Route 
-            path="/DrumMachine" 
+            }
+          />
+          <Route
+            path="/DrumMachine"
             element={
               <AnimatedPage>
                 <DrumMachineApp /> {/* Changed from DrumMachine */}
               </AnimatedPage>
-            } 
+            }
           />
-          <Route 
-            path="/terminal" 
+          <Route
+            path="/terminal"
             element={
               <AnimatedPage>
                 <App />
               </AnimatedPage>
-            } 
+            }
           />
-          {/* Catch-all route */}
-          <Route 
-            path="*" 
+          {/* Catch-all route - this is important for GitHub Pages */}
+          <Route
+            path="*"
             element={
               <AnimatedPage>
                 <Home />
               </AnimatedPage>
-            } 
+            }
           />
         </Routes>
       </AnimatePresence>
     </div>
-  )
+  );
 }
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AppRouter />
     </BrowserRouter>
   </StrictMode>
-)
+);
